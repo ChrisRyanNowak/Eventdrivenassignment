91f581701220a767b3e8d84f5420946dc77ada9b
ChrisRyanNowak <115913612+ChrisRyanNowak@users.noreply.github.com>
2022-11-07 10:09:08 -0700

    Add files via upload

---
+++ Eventdriven.js
@@ -0,0 +1,9 @@
+function disablefirstbutton() {
+  document.getElementById("Start").disabled = true;
+  document.getElementById("Stop").disabled = false;
+}
+
+function disablesecondbutton() {
+  document.getElementById("Stop").disabled = true;
+  document.getElementById("Start").disabled = false;
+}
