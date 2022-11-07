36ab490bf37945e6bf1751121951832fbd0b678a
ChrisRyanNowak <115913612+ChrisRyanNowak@users.noreply.github.com>
2022-11-07 10:42:32 -0700

    Add files via upload

--- Eventdriven.js
+++ Eventdriven.js
@@ -1,9 +1,12 @@
+//creates funtion for button onclick 
 function disablefirstbutton() {
+  //disables start button and enables stop button
   document.getElementById("Start").disabled = true;
   document.getElementById("Stop").disabled = false;
 }
-
+//creates funtion for button onclick 
 function disablesecondbutton() {
+  //disables stop button and enables start button
   document.getElementById("Stop").disabled = true;
   document.getElementById("Start").disabled = false;
 }
